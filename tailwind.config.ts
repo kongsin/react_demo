a92cafbe4bf6c9fccaee9790452af8e45667f6fe
Kongsin Pansansou <pansansou@gmail.com>
2025-03-01 03:51:50 +0700

    feat:  set custom color.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customs:{
+          'cyan-150': '#E5F9FF',
+          'fg-status-void': '#a855f7',
+          'fg-status-accept': '#22C55E',
+          'fg-status-ready-to-send': '#3B82F6',
+          'fg-status-awaiting-aml': '#F97316',
+          'bg-status-void': '#F3E8FF',
+          'bg-status-accept': '#D1FAE5', 
+          'bg-status-ready-to-send': '#DBEAFE',
+          'bg-status-awaiting-aml': '#FFECD1',  
+        }
       },
     },
   },
